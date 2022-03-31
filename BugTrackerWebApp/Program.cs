fd2d4ebad8eebbad31e254ce00c4da62310bb75c
Jozsef123 <jfeher02@gmail.com>
2022-03-31 14:36:37 -0700

    web builder

--- BugTrackerWebApp/Program.cs
+++ BugTrackerWebApp/Program.cs
@@ -66,7 +66,7 @@ namespace BugTrackerWebApp
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://*:" + Environment.GetEnvironmentVariable("PORT"));
+                    webBuilder.UseUrls("https://bug-tracker-web-app-jozsef.herokuapp.com/");
                 });
     }
 }
