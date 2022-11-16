e4362dec3c53c8db67fdb8ea0525e233e93e0166
Jozsef123 <jfeher02@gmail.com>
2022-11-15 16:54:52 -0800

    Feature: Google Oauth2 Authentication

--- Startup.cs
+++ Startup.cs
@@ -81,6 +81,7 @@ namespace BugTrackerWebApp
             {
                 googleOptions.ClientId = Environment.GetEnvironmentVariable("GOOGLE__CLIENTID");
                 googleOptions.ClientSecret = Environment.GetEnvironmentVariable("GOOGLE__CLIENTSECRET");
+                googleOptions.SignInScheme = IdentityConstants.ExternalScheme;
             });
         }
         //
